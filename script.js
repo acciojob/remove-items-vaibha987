2d61ef7e0826925037c409b43b855ba51270ac43
vaibha987 <v2goyal95@gmail.com>
2024-07-27 14:07:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function SelectRemove(){
+let del=document.getElementById("colorSelect")
+	 let selectedIndex = del.selectedIndex;
+    del.remove(selectedIndex);
+}
+
